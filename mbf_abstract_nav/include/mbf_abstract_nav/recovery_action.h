e74d742e8619e61d35d44bbacc1e00db39dfce35
Sebastian Pütz <spuetz@uos.de>
2018-09-11 17:39:24 +0200

    Rebase from 'upstream'

--- mbf_abstract_nav/include/mbf_abstract_nav/recovery_action.h
+++ mbf_abstract_nav/include/mbf_abstract_nav/recovery_action.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017, Magazino GmbH, Sebastian Pütz, Jorge Santos Simón
+ *  Copyright 2018, Magazino GmbH, Sebastian Pütz, Jorge Santos Simón
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  *
- *  simple_controller_execution.cpp
+ *  recovery_action.h
  *
  *  authors:
  *    Sebastian Pütz <spuetz@uni-osnabrueck.de>
@@ -38,48 +38,31 @@
  *
  */
 
-#include "mbf_simple_nav/simple_controller_execution.h"
-#include <pluginlib/class_loader.h>
+#ifndef MBF_ABSTRACT_NAV__RECOVERY_ACTION_H_
+#define MBF_ABSTRACT_NAV__RECOVERY_ACTION_H_
 
-namespace mbf_simple_nav
-{
+#include "mbf_abstract_nav/abstract_action.h"
+#include "mbf_abstract_nav/abstract_recovery_execution.h"
+#include "mbf_abstract_nav/robot_information.h"
+#include <actionlib/server/action_server.h>
+#include <mbf_msgs/RecoveryAction.h>
+#include <boost/thread/condition_variable.hpp>
 
-SimpleControllerExecution::SimpleControllerExecution(boost::condition_variable &condition,
-                                                     const boost::shared_ptr<tf::TransformListener> &tf_listener_ptr) :
-    mbf_abstract_nav::AbstractControllerExecution(condition, tf_listener_ptr)
+namespace mbf_abstract_nav{
+
+class RecoveryAction : public AbstractAction<mbf_msgs::RecoveryAction, AbstractRecoveryExecution>
 {
+ public:
+
+  typedef boost::shared_ptr<RecoveryAction> Ptr;
+
+  RecoveryAction(const std::string& name, const RobotInformation &robot_info);
+
+  void run(GoalHandle &goal_handle, AbstractRecoveryExecution &execution);
+
+};
+
 }
 
-mbf_abstract_core::AbstractController::Ptr SimpleControllerExecution::loadControllerPlugin(
-    const std::string& controller_type)
-{
-  static pluginlib::ClassLoader<mbf_abstract_core::AbstractController>
-      class_loader("mbf_abstract_core", "mbf_abstract_core::AbstractController");
-  mbf_abstract_core::AbstractController::Ptr controller_ptr;
-  ROS_DEBUG("Load controller plugin.");
-  try
-  {
-    controller_ptr = class_loader.createInstance(controller_type);
-    ROS_INFO_STREAM("MBF_core-based local planner plugin " << controller_type << " loaded");
-  }
-  catch (const pluginlib::PluginlibException &ex)
-  {
-    ROS_FATAL_STREAM("Failed to load the " << controller_type
-        << " local planner, are you sure it's properly registered"
-        << " and that the containing library is built? Exception: " << ex.what());
-  }
-  return controller_ptr;
-}
 
-bool SimpleControllerExecution::initPlugin(
-    const std::string& name,
-    const mbf_abstract_core::AbstractController::Ptr& controller_ptr)
-{
-  return true;
-}
-
-SimpleControllerExecution::~SimpleControllerExecution()
-{
-}
-
-} /* namespace move_base_nav_moving */
+#endif //MBF_ABSTRACT_NAV__RECOVERY_ACTION_H_
